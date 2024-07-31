5155e8841da1061640d1f2d6af00be1515169718
kelvinComputing <kelvincontrera1@gmail.com>
2024-07-31 15:40:13 -0400

    modificaciones para que funcione con importaciones y exportaciones de tipo ES6

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
-const { envs } = require('./config/env')
-const { startServer } = require('./server/server')
+import { envs } from "./config/env.js";
+import { startServer } from "./server/server.js";
 
-
+// Version Vieja
+// const { envs } = require('./config/env')
+// const { startServer } = require('./server/server')
 
 const main = () => {
-    startServer({
-        port: envs.PORT,
-        public_path: envs.PUBLIC_PATH
-    })
-}
+  startServer({
+    port: envs.PORT,
+    public_path: envs.PUBLIC_PATH,
+  });
+};
 
 (async () => {
-    main()
-})()
+  main();
+})();
--- src/server/server.js
+++ src/server/server.js
@@ -1,24 +1,48 @@
-const { log } = require('console')
-const express = require('express')
-const path = require('path')
+import express from "express";
+import path from "path";
 
-const startServer = (options) => {
-    const { port, public_path = 'public' } = options
+// Forma vieja
+// const express = require('express')
+// const path = require('path')
 
-    const app = express()
+export const startServer = (options) => {
+  const { port, public_path = "public" } = options;
 
-    app.use(express.static(public_path))
+  const app = express();
 
-    app.get('*', (req, res) => {
-        const indexPath = path.join(__dirname + `../../../${public_path}/index.html`)
-        res.sendFile(indexPath)
-    })
+  app.use(express.static(public_path));
 
-    app.listen(port, () => {
-        console.log(`escuchando en el puerto ${port}`);
-    })
-}
+  app.get("*", (req, res) => {
+    const indexPath = path.join(
+      __dirname + `../../../${public_path}/index.html`
+    );
+    res.sendFile(indexPath);
+  });
 
-module.exports = {
-    startServer
-}
+  app.listen(port, () => {
+    console.log(`escuchando en el puerto ${port}`);
+  });
+};
+
+// const startServer = (options) => {
+//   const { port, public_path = "public" } = options;
+
+//   const app = express();
+
+//   app.use(express.static(public_path));
+
+//   app.get("*", (req, res) => {
+//     const indexPath = path.join(
+//       __dirname + `../../../${public_path}/index.html`
+//     );
+//     res.sendFile(indexPath);
+//   });
+
+//   app.listen(port, () => {
+//     console.log(`escuchando en el puerto ${port}`);
+//   });
+// };
+
+// module.exports = {
+//   startServer,
+// };
